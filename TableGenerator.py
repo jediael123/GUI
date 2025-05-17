df65a3e5f9280bd872d41cec96f6fb45d290df1e
jediael123 <priscilladear1@yahoo.com>
2025-05-17 18:57:09 +0100

    Add files via upload

--- TableGenerator.py
+++ TableGenerator.py
@@ -1,1 +1,25 @@
-import _tkinter
+from tkinter import*
+
+root = Tk()
+root.title("Table Generator")
+root.geometry("400x400")
+root.config(bg="blue")
+
+main_label = Label(root,text="Table Generator",bg="black",fg="white")
+main_label.grid(row=0,column=0,columnspan=3,pady=25)
+
+gen_label = Label(root,text="table",bg="black",fg="white")
+gen_label.grid(row=3,column=0)
+
+Generate = Button(root,bg="black",fg="white",width=10)
+Generate.grid(row=10,column=3)
+
+
+
+
+
+
+
+
+
+root.mainloop()
